e348480c5429803f9bb7e28dfbe9235f846022d1
Xinman Liu <xinman2@ualberta.ca>
2022-10-14 16:07:31 -0400

    Fix bugs in exercise

--- exercise.py
+++ exercise.py
@@ -10,26 +10,24 @@ import string
 import pickle
 import math
 import numpy as np
-from numpy.linalg import norm
 from scipy.stats import pearsonr
-from sklearn.decomposition import TruncatedSVD
+from sklearn.decomposition import PCA
+from sklearn.metrics.pairwise import cosine_similarity
 
 
-def preprocess():
-    translator = str.maketrans("", "", string.punctuation)
-    stop_words = set(stopwords.words("english"))
-    clean_text = []
-    for word in brown.words():
-        word = word.lower()
-        if (
-            word not in stop_words and word.translate(translator) != ""
-        ):  # remove stopwords and punctuation
-            clean_text.append(word)
-    return clean_text
+def contain_punc(word):
+    punctuation = [p for p in string.punctuation]
+    for p in punctuation:
+        if p in word:
+            return True
+    return False
 
 
 def generate_vocab():
-    text = preprocess()
+    stop_words = stopwords.words("english")
+    text = [
+        w.lower() for w in brown.words() if not contain_punc(w) and w not in stop_words
+    ]  # remove punc and stopwords
     fDist = nltk.FreqDist(text)
     fDist_5000 = fDist.most_common(5000)
     print("the 5 most common words: \n", fDist_5000[:5])
@@ -40,38 +38,18 @@ def generate_vocab():
 
     fDist_5000_w = set(list(zip(*fDist_5000))[0])
 
-    # find the intersection between W and table1 - P and S
-    intersect = fDist_5000_w.intersection(table1)
-    with open("table1_sim.plk", "rb") as f:
-        table1_sim = pickle.load(f)
-
-    i = 0
-    s = []
-    p = {}
-    for key in table1_sim.keys():
-        v, w = key
-        if v in intersect and w in intersect:
-            p[(v, w)] = i
-            s.append(table1_sim[key])
-            i += 1
-
     vocab = list(fDist_5000_w.union(table1))
     print(f"|W| = {len(vocab)}.\n")
 
-    return vocab, p, np.array(s)
-
-
-def calc_sim(m, v, w, vocab_index):
-    vec_v = m[vocab_index[v], :]
-    vec_w = m[vocab_index[w], :]
-    return np.matmul(vec_v, vec_w) / (norm(vec_v, 2) * norm(vec_w, 2))
+    return vocab
 
 
 if __name__ == "__main__":
-    vocab, p, s = generate_vocab()
+    # Step 2: extract the 5000 most common words
+    vocab = generate_vocab()
     vocab_index = {vocab[i]: i for i in range(len(vocab))}
 
-    # word-context matrix
+    # Step 3: word-context matrix
     corpus = brown.words()
     m1 = np.zeros((len(vocab), len(vocab)))
     for i in range(len(corpus) - 1):
@@ -82,7 +60,7 @@ if __name__ == "__main__":
             if next_w != cur_w:
                 m1[vocab_index[next_w], vocab_index[cur_w]] += 1
 
-    # PPMI
+    # Step4: PPMI
     m1_plus = np.zeros_like(m1)
     total = np.sum(m1)
     marginal = np.sum(m1, axis=0)
@@ -95,38 +73,56 @@ if __name__ == "__main__":
             p_w = marginal[vocab_index[w]] / total
             m1_plus[vocab_index[v], vocab_index[w]] = max(math.log2(p_v_w / (p_v * p_w)), 0)
 
-    # Latent Semantic Analysis
-    svd_10 = TruncatedSVD(n_components=10)
+    # Step 5: Latent Semantic Analysis
+    svd_10 = PCA(n_components=10)
     m2_10 = svd_10.fit_transform(m1_plus)
 
-    svd_100 = TruncatedSVD(n_components=100)
+    svd_100 = PCA(n_components=100)
     m2_100 = svd_100.fit_transform(m1_plus)
 
-    svd_300 = TruncatedSVD(n_components=300)
+    svd_300 = PCA(n_components=300)
     m2_300 = svd_300.fit_transform(m1_plus)
 
-    # calculate similarity
+    # Step 6: find the intersection between W and table1 - P and S
+    with open("table1_sim.plk", "rb") as f:
+        table1_sim = pickle.load(f)
+
+    i = 0
+    s = []
+    p = {}
+    for key in table1_sim.keys():
+        v, w = key
+        if v in vocab and w in vocab:
+            p[(v, w)] = i
+            s.append(table1_sim[key])
+            i += 1
+
+    # Step 7: calculate similarity
     s_m1 = np.zeros_like(s)
-    for v, w in p.keys():
-        s_m1[p[(v, w)]] = calc_sim(m1, v, w, vocab_index)
-    print(f"Pearson correlation between s and s_m1: {np.corrcoef(s, s_m1)[0, 1]}")
-
     s_m1_plus = np.zeros_like(s)
-    for v, w in p.keys():
-        s_m1_plus[p[(v, w)]] = calc_sim(m1_plus, v, w, vocab_index)
-    print(f"Pearson correlation between s and s_m1_plus: {np.corrcoef(s, s_m1_plus)[0, 1]}")
-
     s_m2_10 = np.zeros_like(s)
-    for v, w in p.keys():
-        s_m2_10[p[(v, w)]] = calc_sim(m2_10, v, w, vocab_index)
-    print(f"Pearson correlation between s and s_m2_10: {np.corrcoef(s, s_m2_10)[0, 1]}")
-
     s_m2_100 = np.zeros_like(s)
-    for v, w in p.keys():
-        s_m2_100[p[(v, w)]] = calc_sim(m2_100, v, w, vocab_index)
-    print(f"Pearson correlation between s and s_m2_100: {np.corrcoef(s, s_m2_100)[0, 1]}")
-
     s_m2_300 = np.zeros_like(s)
     for v, w in p.keys():
-        s_m2_300[p[(v, w)]] = calc_sim(m2_300, v, w, vocab_index)
-    print(f"Pearson correlation between s and s_m2_300: {np.corrcoef(s, s_m2_300)[0, 1]}")
+        s_m1[p[(v, w)]] = cosine_similarity(
+            m1[vocab_index[v]].reshape(1, -1), m1[vocab_index[w]].reshape(1, -1)
+        )[0][0]
+        s_m1_plus[p[(v, w)]] = cosine_similarity(
+            m1_plus[vocab_index[v]].reshape(1, -1), m1_plus[vocab_index[w]].reshape(1, -1)
+        )[0][0]
+        s_m2_10[p[(v, w)]] = cosine_similarity(
+            m2_10[vocab_index[v]].reshape(1, -1), m2_10[vocab_index[w]].reshape(1, -1)
+        )[0][0]
+        s_m2_100[p[(v, w)]] = cosine_similarity(
+            m2_100[vocab_index[v]].reshape(1, -1), m2_100[vocab_index[w]].reshape(1, -1)
+        )[0][0]
+        s_m2_300[p[(v, w)]] = cosine_similarity(
+            m2_300[vocab_index[v]].reshape(1, -1), m2_300[vocab_index[w]].reshape(1, -1)
+        )[0][0]
+
+    # Step 8: report pearson correlation
+    print(f"Pearson correlation between s and s_m1: {pearsonr(s, s_m1)}")
+    print(f"Pearson correlation between s and s_m1_plus: {pearsonr(s, s_m1_plus)}")
+    print(f"Pearson correlation between s and s_m2_10: {pearsonr(s, s_m2_10)}")
+    print(f"Pearson correlation between s and s_m2_100: {pearsonr(s, s_m2_100)}")
+    print(f"Pearson correlation between s and s_m2_300: {pearsonr(s, s_m2_300)}")
